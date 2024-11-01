9c5427471c0c961eadf0ba3715423af0554e48e6
Michal2001 <testEmail@host.com>
2024-11-01 20:28:08 +0100

    Added custom login form using Bootstrap 5

--- index.php
+++ index.php
@@ -1,3 +1,67 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Książki</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-4 offset-lg-4 text-center my-5">
+                <h1>Zaloguj się</h1>
+                <form action="index.php" method = "post">
+                        <div class="form-group text-start">
+                            <label for="emailInput">Adres e-mail:</label>
+                            <input type="email" class="form-control" name="email" id="emailInput" aria-describedby="emailHelp">
+                            <small id="emailHelp" class="form-text text-muted">Nie przekazujemy twojego adresu e-mail innym podmiotom.</small>
+                        </div>
+                        <div class="form-group text-start">
+                            <label for="passwordInput">Hasło:</label>
+                            <input type="password" class="form-control" name="password" id="passwordInput">
+                        </div>
+                        <div class="form-group form-check text-start">
+                            <input type="checkbox" class="form-check-input" id="rememberMeButton">
+                            <label class="form-check-label" for="rememberMeButton">Zapamiętaj mnie</label>
+                            
+                        </div>
+                        <input type="hidden" name="action" value="login"></input>
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary btn-block">Zaloguj</button>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+   
+    
+    
+    <h1>Zarejestruj się</h1>
+    <form action="index.php" method="post">
+        <label for="emailInput">Email:</label>
+        <input type="email" name="email" id="emailInput"><br>
+
+        <label for="passwordInput">Hasło:</label>
+        <input type="password" name="password" id="passwordInput"><br>
+
+        <label for="passwordRepeatInput">Hasło ponownie:</label>
+        <input type="password" name="passwordRepeat" id="passwordRepeatInput"><br>
+
+        <input type="hidden" name="action" value="register"></input>
+        <input type="submit" value="Zarejestruj">
+    </form>
+   
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
 <?php
 if(isset($_REQUEST['action']) && $_REQUEST['action']=="login"){
 $email = $_REQUEST['email'];
@@ -12,7 +76,6 @@ $db = new mysqli('localhost','root','','auth');
 $q = $db->prepare("SELECT * FROM user WHERE email = ? LIMIT 1"); //limit do jednego wiersza
 //podstaw wartosci
 $q->bind_param("s",$email);
-// $q->bind_param("s",);
 //wykonaj
 $q->execute();
 $result_login = $q -> get_result();
@@ -22,7 +85,7 @@ if($userRow == null){
     echo "Błędny login lub hasło <br>"; //konto nie istnieje lub błędny login/hasło
 }else{
     //konto istnieje
-    if(password_verify($password, $userRow['passwordHash'])){
+    if(password_verify($password, $userRow['passwordHash'])){ //password_verify sprawdza hashe
         //haslo poprawne
         echo "Zalogowano poprawnie <br>";
     }
@@ -54,36 +117,7 @@ if(isset($_REQUEST['action']) && $_REQUEST['action']=="register"){
     }
     
 }
-//
-
 
 // email michal2001@czytaj.pl
 // hasło tajneHaslo
 ?>
-<title>Książki</title>
-<h1>Zaloguj się</h1>
-<form action="index.php" method = "get">
-    <label for="emailInput">Email:</label>
-    <input type="email" name="email" id="emailInput"><br>
-    <label for="passwordInput">Hasło:</label>
-    <input type="password" name="password" id="passwordInput"><br>
-    <input type="hidden" name="action" value="login"></input>
-    <input type="submit" value="Zaloguj">
-</form>
-
-<h1>Zarejestruj się</h1>
-<form action="index.php" method="post">
-    <label for="emailInput">Email:</label>
-    <input type="email" name="email" id="emailInput"><br>
-
-    <label for="passwordInput">Hasło:</label>
-    <input type="password" name="password" id="passwordInput"><br>
-
-    <label for="passwordRepeatInput">Hasło ponownie:</label>
-    <input type="password" name="passwordRepeat" id="passwordRepeatInput"><br>
-
-    <input type="hidden" name="action" value="register"></input>
-    <input type="submit" value="Zarejestruj">
-</form>
-
-
