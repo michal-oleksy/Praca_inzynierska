ba8d4d5a4c5753d587076242d5515c8646f54eba
Michal2001 <testEmail@host.com>
2024-11-02 18:36:08 +0100

    new file:   .gitignore modified:   piaskownica.php

--- piaskownica.php
+++ piaskownica.php
@@ -1,36 +1,5 @@
 <?php
-declare(strict_types=1);
 
-//              funkcja callable
-// function myCall($name, callable $func):void{
-//     $func($name);
-// }
-
-
-// $myFunction = function($name){
-//     echo $name;
-// };
-
-// myCall("michal",$myFunction);
-//              funkcja callable
-
-
-//      funkcje strzałkowe
-// $a = fn($value) => $value*2;
-// echo $a(2345);
-$users=[
-'a','b','c'
-];
-
-$hello = array_map(
-    fn($value) => $value.' hej',
-    $users
-);
-print_r ($hello);
-
-foreach($users as $value){
-    print_r ($value." hej\n");
-}
 
 
 
